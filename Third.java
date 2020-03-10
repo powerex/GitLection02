d0047d852151e1447641369baf0cf1a0cf041d1e
powerex <flaxazbest@gmail.com>
2020-03-10 10:27:57 +0200

    new file Third.java

--- Third.java
+++ Third.java
@@ -1,9 +1,7 @@
-public class Main {
+public class Third {
 	public static void main(String[] args) {
 		System.out.pintln("Hello Git");
 		System.out.pintln("Hello Git two times");
 		System.out.pintln("Hello Git again");
-		System.out.pintln("Hello Git and again");
-		
 	}
 }
